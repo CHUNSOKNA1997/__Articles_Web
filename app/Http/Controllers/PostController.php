e9c290a0fe499f9e75ff6648bb959e16cbb79fd4
Chun SOKNA <chun.sokna.1222@gmail.com>
2025-05-27 19:48:57 +0700

    Added dashboard page with basic route and controller
    - Create new Dashboard/Index.jsx component
    - Remove unused CommentController route
    - Update PostController to render dashboard view using Inertia

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 class PostController extends Controller
 {
     //
+
+    public function index()
+    {
+        return Inertia::render('Dashboard/Index');
+    }
 }
---
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Index = () => {
+    return <div>This is dashboard page</div>;
+};
+
+export default Index;
